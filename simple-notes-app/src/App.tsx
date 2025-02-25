5f04845fb3ccc23572973767c28b2a5430ef606b
Murtada Ahmed <murtadhabg@gmail.com>
2025-02-25 21:37:06 +0200

    added backend

--- simple-notes-app/src/App.tsx
+++ simple-notes-app/src/App.tsx
@@ -1,11 +1,27 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {Note} from "./types.ts";
 import NoteForm from "./components/NoteForm.tsx";
 import NoteList from "./components/NoteList.tsx";
 import './styles/App.css'
 
+const API_URL = "http://localhost:5000/note"
+
 function App() {
     const [notes, setNotes] = useState<Note[]>([])
+
+    useEffect(() => {
+        fetch(API_URL)
+            .then((res) => res.json())
+            .then(setNotes)
+    }, [])
+
+    useEffect(() => {
+        fetch(API_URL, {
+            method: "POST",
+            headers: {"content-type": "application/json"},
+            body: JSON.stringify(notes)
+        })
+    }, [notes]);
 
     const addNote = (title: string, text: string) => {
         const newNote: Note = {
